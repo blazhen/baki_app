4eca5c2e9f41ac710c9756118480820f6076886a
blazhen <55562862+blazhen@users.noreply.github.com>
2020-01-27 16:35:52 +0100

    Done but needs more stylishing

--- recipes/src/components/SingleRecipe.js
+++ recipes/src/components/SingleRecipe.js
@@ -6,15 +6,7 @@ export default class SingleRecipe extends React.Component{
         super(props)
         
     }
-        //     getInstructionSNumber = () => {
-        //        var number = []
-        //         for(let i = 0; i<this.props.data.analyzedInstructions[0].steps.length; i++){
-        //              number.push(<li>{this.props.data.analyzedInstructions[0].steps[i].number}</li>)
-        //              console.log(this.props.data.analyzedInstructions[0].steps[i].number)   
-        //         }
-        //         return number
-        // }
-        getInstructionSteps = () => {
+     getInstructionSteps = () => {
             var steps = []
              for(let i = 0; i<this.props.data.analyzedInstructions[0].steps.length; i++){
                   steps.push(<li>{this.props.data.analyzedInstructions[0].steps[i].step}</li>)
@@ -22,42 +14,64 @@ export default class SingleRecipe extends React.Component{
              }
              return steps
      }
+     getIngredients = () => {
+        var ingredients = []
+         for(let i = 0; i<this.props.data.extendedIngredients.length; i++){
+              ingredients.push(<li>{this.props.data.extendedIngredients[i].name}</li>)
+              console.log(this.props.data.extendedIngredients[i])   
+         }
+         return ingredients
+ }
 
              render(){
                 
                 return(
                     <React.Fragment>
                     <div className="recipe-container">
-                <div className="recipe-info">
+                <div className="recipes-info">
+                    <div className="recipe-info">
                         <div className="recipes-title">
-                         <h1>{this.props.data.title}</h1>
-                         <div>{!this.props.data.vegeterian ? <div>Not Vegeterian</div> : <div>Vegeterian</div>}</div>
+                         <h1>{this.props.data.title}</h1>                         
                      </div>
                      <div className="recipes-img">
                          <img src={this.props.data.image} alt="recipe"></img>
                      </div>                
-                     <div className="recipes-footer">
+                        <div className="recipes-footer">
                          <div className="recipes-clock">
-                         <i className="far fa-clock"><span>This recipe can be ready in:{this.props.data.readyInMinutes}</span></i>
+                         <i className="far fa-clock"><span> This recipe can be ready in: {this.props.data.readyInMinutes} mins.</span></i>
                          </div>
                          <div className="recipes-food">
-                <i className="fas fa-utensils"><span>This recipe can serve up to:{this.props.data.servings}</span></i>
+                <i className="fas fa-utensils"><span> This recipe can serve up to: {this.props.data.servings}</span></i>
                          </div>
+                         <div className="preparation-mins">
+                <i className="fas fa-clock"><span> Preparation time: {this.props.data.preparationMinutes} mins.</span></i>
+                         </div>
+                         <div className="cooking-mins">
+                <i className="fas fa-stopwatch"><span> Cooking mins: {this.props.data.cookingMinutes}</span></i>
+                         </div>
+                        </div>
+                        <div>
+                            {!this.props.data.Vegetarian ? <div>Not Vegeterian</div> : <div>Vegeterian</div>}
+                        </div>
                      </div>
                 </div>
-                     <div className="ingredients-container">
-                          <div>
-                              <ul>
-                                  {this.getInstructionSteps()}
-                              </ul>
-                            </div>
-                     </div>
-               
-                     
-                    
+                <div className="ingredients-container">                    
+                    <h1>Ingredients</h1>
+                <ol>
+                    {this.getIngredients()}
+                    </ol>                    
+               </div>                                                       
              </div>
-             
-             
+             <div className="instruction-container">   
+                <div className="sbs-container">
+             <h1>Step by Step</h1>                       
+                    <ol>
+                        {this.getInstructionSteps()}                                  
+                    </ol>   
+                </div>                       
+            </div> 
+                              
+                    
              </React.Fragment>
                 )
              }
