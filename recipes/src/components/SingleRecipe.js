ffee8ae7fa321401b413de8af55bd38ff57b24e6
blazhen <55562862+blazhen@users.noreply.github.com>
2020-01-27 18:47:27 +0100

    something done

--- recipes/src/components/SingleRecipe.js
+++ recipes/src/components/SingleRecipe.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import '../assets/SingleRecipe.css'
-import Sliderce from './Sliderce'
 
 export default class SingleRecipe extends React.Component{ 
     constructor(props){
         super(props)
         
     }
+     
      getInstructionSteps = () => {
             var steps = []
              for(let i = 0; i<this.props.data.analyzedInstructions[0].steps.length; i++){
@@ -29,50 +29,52 @@ export default class SingleRecipe extends React.Component{
                 return(
                     <React.Fragment>
                         
-                    <div className="recipe-container">
-                <div className="recipes-info">
-                    <div className="recipe-info">
-                        <div className="recipes-title">
-                         <h1>{this.props.data.title}</h1>                         
+<div className="recipe-container">
+    <div className="recipes-info">
+        <div className="recipe-info">
+            <div className="recipes-title">
+                <h1>{this.props.data.title}</h1>                         
+            </div>
+                <div className="recipes-img">
+                    <img src={this.props.data.image} alt="recipe"></img>
+                </div>                
+                    <div className="recipes-footer">
+                        <div className="recipes-clock">
+                            <i className="far fa-clock"><span> This recipe can be ready in: {this.props.data.readyInMinutes} mins.</span></i>
                         </div>
-                     <div className="recipes-img">
-                         <img src={this.props.data.image} alt="recipe"></img>
-                     </div>                
-                        <div className="recipes-footer">
-                         <div className="recipes-clock">
-                         <i className="far fa-clock"><span> This recipe can be ready in: {this.props.data.readyInMinutes} mins.</span></i>
-                         </div>
-                         <div className="recipes-food">
-                <i className="fas fa-utensils"><span> This recipe can serve up to: {this.props.data.servings} people.</span></i>
-                         </div>
-                         <div className="preparation-mins">
-                <i className="fas fa-clock"><span> Preparation time: {this.props.data.preparationMinutes} mins.</span></i>
-                         </div>
-                         <div className="cooking-mins">
-                <i className="fas fa-stopwatch"><span> Cooking: {this.props.data.cookingMinutes} mins.</span></i>
-                         </div>
-                        </div>
-                        <div className="vegeterian">
+                            <div className="recipes-food">
+                                <i className="fas fa-utensils"><span> This recipe can serve up to: {this.props.data.servings} people.</span></i>
+                            </div>
+                                <div className="preparation-mins">
+                                    <i className="fas fa-clock"><span> Preparation time: {this.props.data.preparationMinutes} mins.</span></i>
+                                </div>
+                                    <div className="cooking-mins">
+                                        <i className="fas fa-stopwatch"><span> Cooking: {this.props.data.cookingMinutes} mins.</span></i>
+                                    </div>
+                    </div>                                                
+        </div>
+                     <div className="something">
                             <h1>{!this.props.data.Vegetarian ? <div>Not Vegeterian</div> : <div>Vegeterian</div>}</h1>
                             <h1>{!this.props.data.dairyFree ? <div>Not Dairy Free</div> : <div>Is Diary Free</div>}</h1>
                         </div>                        
-                     </div>
-                </div>
-                <div className="ingredients-container">                    
-                    <h1>Ingredients</h1>
-                <ol>
-                    {this.getIngredients()}
-                    </ol>                    
-               </div>                                                       
-             </div>
-             <div className="instruction-container">   
-                <div className="sbs-container">
+                        
+    </div>                
+                    <div className="ingredients-container">                    
+                        <h1>Ingredients</h1>
+                        <ol>
+                            {this.getIngredients()}
+                        </ol>                    
+                     </div>                                                       
+</div>
+    <div className="instruction-container">   
+        <div className="sbs-container">
              <h1>Step by Step</h1>                       
-                    <ol>
-                        {this.getInstructionSteps()}                                  
-                    </ol>   
-                </div>                       
-            </div>            
+                <ol>
+                    {this.getInstructionSteps()}                                  
+                </ol>   
+        </div>    
+                         
+    </div>            
                               
                     
              </React.Fragment>
